7d6519ac618639add7e4be469a21cab6861678cd
Miistiik <105573310+Lizb3th09@users.noreply.github.com>
2023-10-22 15:22:56 -0800

    ya casi

--- app/dummyapi/[id]/page.jsx
+++ app/dummyapi/[id]/page.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 
 async function obtnerDatoDummy(id){
-    const response = await fetch(`http://localhost:3000/dummyapi/${id}`)
+    const response = await fetch(`https://dummyjson.com/products/${id}`)
     if(! response.ok) throw Error('no se puede')
     return response.json()
 }
 
 const page = async ({params}) => {
     const {id} = params
-    const data = await obtnerDatoDummy(id)
+    const products = await obtnerDatoDummy(id)
 
     return (
-    <div className='bg-black text-white'>
-        {data.title}
-        
-   
+    <div className='p-8 mx-auto text-center max-w-3xl  bg-zinc-900 rounded-md '>
+
+        <div className='bg-white'> 
+
+    <p className='font-serif text-3xl p-3'> {products.title}</p>
+    <p className=' p-3'>{products.images}</p>
+    <p className='font-serif  p-2'>{products.description}</p>
+
+        </div>
+
+
     </div>
 
   )
