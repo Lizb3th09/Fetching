34ffb5a6bc70d57f9f46f4ab5878cdf89b2576bb
Miistiik <105573310+Lizb3th09@users.noreply.github.com>
2023-10-22 16:19:04 -0800

    3

--- app/dummyapi/[id]/page.jsx
+++ app/dummyapi/[id]/page.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import TarjetaProductoDummy from '@/app/src/components/TarjetaProductoDummy';
+import React from 'react';
 
-async function obtnerDatoDummy(id){
-    const response = await fetch(`https://dummyjson.com/products/${id}`)
-    if(! response.ok) throw Error('no se puede')
-    return response.json()
+async function obtnerDatoDummy(id) {
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  if (!response.ok) throw Error('No se puede obtener el producto');
+  return response.json();
 }
 
-const page = async ({params}) => {
-    const {id} = params
-    const products = await obtnerDatoDummy(id)
+const page = async ({ params }) => {
+  const { id } = params;
+  const product = await obtnerDatoDummy(id);
 
-    return (
-    <div className='p-8 mx-auto text-center max-w-3xl  bg-zinc-900 rounded-md '>
+  if (!product) {
+    return <div>Error: no se puede obtener el producto</div>;
+  }
 
-        <div className='bg-white'> 
+  return (
+    <div className='p-11 mx-auto text-center max-w-3xl bg-zinc-900 rounded-md '>
+      <div className='border-md flex  flex-col  mt-5 rounded-md bg-white items-center mx-auto max-w-md  p-8 font-serif '>
+        <p className='font-serif text-3xl p-3 ' >{product.title}</p>
 
-    <p className='font-serif text-3xl p-3'> {products.title}</p>
-    <p className=' p-3'>{products.images}</p>
-    <p className='font-serif  p-2'>{products.description}</p>
+        <p className='font-serif p-4'>{product.description}</p> 
 
-        </div>
 
+        {product.images && product.images.length > 0 && (
+
+    <div className='flex flex-col justify-center items-center'>
+    {product.images.map((image, index) => (
+      <div key={index} className='m-2'>
+    
+    <img src={image} width={400} height={400} alt={`Product Image ${index}`} />
 
     </div>
+    ))}
+  </div>
+)}
 
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default page
+
+export default page;
